bfd23fa589624316381b539102744b3893462672
owaisalam464 <owaisalam700@gmail.com>
2021-07-06 13:11:12 +0500

    done!

---
+++ src/Actions/types.js
@@ -0,0 +1,7 @@
+export const START_QUIZ = "start_quiz";
+export const NEW_ROUND = "new_round";
+export const ANSWER_QUESTION = "answer_question";
+export const NEW_QUIZ = "new_quiz";
+export const FETCH_REQUEST = "fetch_request";
+export const FETCH_SUCCESS = "fetch_success";
+export const FETCH_ERROR = "fetch_error";
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import "./style/index.css";
+import Root from "./containers/Root";
+import registerServiceWorker from "./registerServiceWorker";
+import rootReducer from "./reducers/index";
+import thunk from "redux-thunk";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk))
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Root store={store} />, document.getElementById("root"));
+registerServiceWorker();
